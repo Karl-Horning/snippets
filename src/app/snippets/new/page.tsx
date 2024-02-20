b7706604af41b0cd63b7d026fa60d09a77a6009e
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-20 22:21:15 +0000

    Added logic to add snippet to database

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,7 +1,33 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
+    const createSnippet = async (formData: FormData) => {
+        // Use server action
+        "use server";
+
+        // Validate inputs
+        const title = formData.get("title") as string;
+        const code = formData.get("code") as string;
+
+        // Create a new database record
+        const snippet = await db.snippet.create({
+            data: {
+                title,
+                code,
+            },
+        });
+
+        // TODO: Remove
+        console.log(snippet);
+
+        // Redirect back to the root route
+        redirect("/");
+    };
+
     return (
         <>
-            <form>
+            <form action={createSnippet}>
                 <h3 className="font-bold m-3">Create a Snippet</h3>
                 <div className="flex flex-col gap-4">
                     {/* Title input */}
@@ -28,10 +54,7 @@ export default function SnippetCreatePage() {
                             name="code"
                         />
                     </div>
-                    <button
-                        className="rounded p-2 bg-blue-200"
-                        type="submit"
-                    >
+                    <button className="rounded p-2 bg-blue-200" type="submit">
                         Create
                     </button>
                 </div>
