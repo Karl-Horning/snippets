9b794c3dc4b69841510900c9865aac2d8723be8f
Karl Horning <32777806+Karl-Horning@users.noreply.github.com>
2024-02-20 23:05:16 +0000

    Added 404 and loading pages

---
+++ src/app/snippets/[id]/loading.tsx
@@ -0,0 +1,9 @@
+export default function SnippetLoadingPage() {
+    return (
+        <>
+            <h1 className="text-xl bold">
+                Loading...
+            </h1>
+        </>
+    );
+}
---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function SnippetNotFound() {
+    return (
+        <>
+            <h1 className="text-xl bold">
+                Sorry, we couldn&apos;t find that particular snippet
+            </h1>
+        </>
+    );
+}
